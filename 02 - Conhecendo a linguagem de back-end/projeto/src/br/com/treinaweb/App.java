0435de3d510300436b2ff8b6c993407d8a3983e0
Daniel Ramos <financdaniel@gmail.com>
2025-01-31 10:35:46 -0300

    Projeto Java finalizado

--- 02 - Conhecendo a linguagem de back-end/projeto/src/br/com/treinaweb/App.java
+++ 02 - Conhecendo a linguagem de back-end/projeto/src/br/com/treinaweb/App.java
@@ -1,15 +1,101 @@
 package br.com.treinaweb;
 
+import java.util.Scanner;
+
 import br.com.treinaweb.models.Diarista;
+import br.com.treinaweb.repository.DiaristaRepository;
 
 public class App {
     public static void main(String[] args) {
-        var diarista = new Diarista(1L, "Maria da Silva", "(11) 99999-9999", "123.123.123-11", "Av Paulista, 1220");
+        var scanner = new Scanner(System.in);
+        var repository = new DiaristaRepository();
 
-        System.out.println(diarista.getId());
-        System.out.println(diarista.getNome());
-        System.out.println(diarista.getTelefone());
-        System.out.println(diarista.getCpf());
-        System.out.println(diarista.getEndereco());
+        var opcao = "q";
+        do {
+            System.out.println();
+
+            exibirMenu();
+
+            System.out.print("Informe a opção desejada: ");
+            opcao = scanner.nextLine();
+
+            System.out.println();
+
+            switch (opcao) {
+                case "1" -> listarDiaristas(repository);
+                case "2" -> cadastrarDiarista(scanner, repository);
+                case "3" -> buscarDiarista(scanner, repository);
+                case "4" -> excluirDiarista(scanner, repository);
+                case "q" -> System.out.println("Saindo do Sistema");
+                default -> System.out.println("Opção Invalida");
+            }                               
+        } while (!opcao.equals("q"));
+                                                
+        scanner.close();
+    }
+
+    private static void excluirDiarista(Scanner scanner, DiaristaRepository repository) {
+        System.out.print("Id: ");
+        var id = scanner.nextLong();
+        scanner.nextLine();
+
+        var foiExcluido = repository.excluirPorId(id);
+
+        if (foiExcluido) {
+            System.out.println("Diarista excluido com sucesso");
+        } else {
+            System.out.println("Diarista não encontrado");
+        }
+    }
+                                                
+                    private static void buscarDiarista(Scanner scanner, DiaristaRepository repository) {
+                        System.out.print("Id: ");
+                        var id = scanner.nextLong();
+                        scanner.nextLine();
+
+                        var diarista = repository.buscarPorId(id);
+
+                        if (diarista != null) {
+                            System.out.println(diarista.getId() + " - " + diarista.getNome());
+                        } else {
+                            System.out.println("Diarista não encontrada");
+                        }
+                    }
+                
+                    private static void cadastrarDiarista(Scanner scanner, DiaristaRepository repository) {
+        System.out.print("Nome: ");
+        var nome = scanner.nextLine();
+        System.out.print("Telefone: ");
+        var telefone = scanner.nextLine();
+        System.out.print("Cpf: ");
+        var cpf = scanner.nextLine();
+        System.out.print("Endereço: ");
+        var endereco = scanner.nextLine();
+
+        var diarista = new Diarista(nome, telefone, cpf, endereco);
+
+        repository.cadastrar(diarista);
+
+        System.out.println("Diarista cadastrada com sucesso");
+    }
+                
+    private static void listarDiaristas(DiaristaRepository repository) {
+        var diaristas = repository.buscarTodos();
+
+        if (diaristas.isEmpty()) {
+            System.out.println("Não exitem diaristas cadastrados");
+        }
+
+        for (Diarista diarista : diaristas) {
+            System.out.println(diarista.getId() + " - " + diarista.getNome());
+        }
+    }
+                
+    private static void exibirMenu() {
+        System.out.println("1 - Listar Diaristas");
+        System.out.println("2 - Cadastrar Diarista");
+        System.out.println("3 - Buscar Diarista");
+        System.out.println("4 - Excluir Diarista");
+        System.out.println("q - Sair do Sistema");
     }
 }
--- 02 - Conhecendo a linguagem de back-end/projeto/src/br/com/treinaweb/models/Diarista.java
+++ 02 - Conhecendo a linguagem de back-end/projeto/src/br/com/treinaweb/models/Diarista.java
@@ -4,6 +4,10 @@ public class Diarista extends Pessoa {
 
     public Diarista() {}
 
+    public Diarista(String nome, String telefone, String cpf, String endereco) {
+        super(null, nome, telefone, cpf, endereco);
+    }
+
     public Diarista(Long id, String nome, String telefone, String cpf, String endereco) {
         super(id, nome, telefone, cpf, endereco);
     }
